b1233333a251aa8cb8256a32e2bfe67d97445335
Daniel Gould <dmgould310@gmail.com>
2020-08-14 19:14:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,13 +54,13 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    file_parts = filename.gsub(/(\.mp3)/, " ")  
+    file_parts = filename.gsub(/(\.mp3)/, "")  
     file_split = file_parts.split(" - ")
     artist = Artist.find_or_create_by_name(file_split[0])
     song = Song.find_or_create_by_name(file_split[1])
     genre = Genre.find_or_create_by_name(file_split[2])
     song.artist = artist 
-    song.genre = genre  
+    song.genre = genre 
     song 
   end 
   
